8b24209f733a3f77ab650318cf0a96297b45957c
loiclo <lecinloic07@gmail.com>
2023-12-04 13:48:45 +0100

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react"
+
 export default function LoginPage() {
+  // state
+  const [inputValue, setInputValue] = useState("")
+
+  //comportements
+  const handleSubmit = (event) => { 
+    event.preventDefault()
+    alert(`Bonjour ${inputValue}`)
+    setInputValue("")
+   }
+
+   const handleChange = (event) => { 
+    setInputValue(event.target.value)
+    }
+  //affichage (render)
   return (
-    <div>LoginPage</div>
+    <div>
+      <h1>Bienvenue chez nous</h1>
+      <br/>
+      <h2>Connectez-vous</h2>
+      <form action="sumbit" onSubmit={handleSubmit}>
+        <input value={inputValue} onChange={handleChange} type="text" placeholder="Entez voter prénom..." required />
+        <button >Accédez à votre espace</button>
+      </form>
+    </div>
   )
 }
+
