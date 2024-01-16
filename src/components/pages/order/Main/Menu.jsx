65b3f5d09e8fddd133306e8689ae26bbdc60ebd8
loiclo <lecinloic07@gmail.com>
2024-01-16 01:14:44 +0100

    ♻️ F05: ORDER > MENU > Create <Product/>

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 import {fakeMenu2 } from "../../../../fakeData/fakeMenu";
 import { theme } from '/src/theme/index.jsx';
+import Product from "./Product";
 
 export default function Menu() {
     const [menu, setMenu] = useState(fakeMenu2)
@@ -9,34 +10,20 @@ export default function Menu() {
   return (
     <MenuStyled className="menu">
         {menu.map((produit) => {
-            return (
-              <div className="produit">
-                <div className="image">
-                  <img src={produit.imageSource} alt={produit.title}/>
-                </div>
-                <div className="info-text">
-                  <div className="title">{produit.title}</div>
-                  <div className="description">
-                  <div className="price">{produit.price} €</div>
-                  <button className="add-button">Ajouter</button>
-                  </div>
-                </div>
-              </div>
-            )
+            return <Product title={produit.title} imageSource={produit.imageSource} price={produit.price} />
         })}
-        
-      </MenuStyled>
+    </MenuStyled>
   )
 }
 
 const MenuStyled = styled.div`
   background: ${theme.colors.background_white};
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-row-gap: 60px;
-  padding: 50px 50px 50px 150px;
+  padding: 50px 50px 150px;
   justify-items: center;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
     .produit{
       background: red;
@@ -44,21 +31,19 @@ const MenuStyled = styled.div`
       height: 330px;
 
       .image{
-      border: 1px solid blue;
+      border: 1px solid fuchsia;
       width: 100px;
       height: auto;
-      
-      
       /* width: 200px;
       height: 145px; */
         img{
           width: 100%;
           height: 100%;
-          justify-content: center;
+          object-fit: cover;
         }
       }
-
+      .description{
+      border: 1px solid fuchsia;
+      }
     }
-
-
-`; 
+`;
---
+++ src/components/pages/order/Main/Product.jsx
@@ -0,0 +1,17 @@
+export default function Product({title, imageSource, price}) {
+  return (
+    <div className="produit">
+        <div className="image">
+            <img src={imageSource} alt={title}/>
+        </div>
+    <div className="info-text">
+      <div className="description">
+        <div className="title">{title}</div>
+        <div className="price">{price} €</div>
+        <button className="add-button">Ajouter</button>
+      </div>
+    </div>
+  </div>
+  )
+}
+
