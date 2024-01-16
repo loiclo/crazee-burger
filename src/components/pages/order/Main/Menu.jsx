c9e98841c60f677874f65f4ca6346cd8b42430c9
loiclo <lecinloic07@gmail.com>
2024-01-16 12:35:25 +0100

    💄 F05: ORDER > Product > add image layout

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -24,26 +24,4 @@ const MenuStyled = styled.div`
   padding: 50px 50px 150px;
   justify-items: center;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-
-    .produit{
-      background: red;
-      width: 240px;
-      height: 330px;
-
-      .image{
-      border: 1px solid fuchsia;
-      width: 100px;
-      height: auto;
-      /* width: 200px;
-      height: 145px; */
-        img{
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-        }
-      }
-      .description{
-      border: 1px solid fuchsia;
-      }
-    }
 `;
--- src/components/pages/order/Main/Product.jsx
+++ src/components/pages/order/Main/Product.jsx
@@ -1,17 +1,49 @@
+import styled from "styled-components";
+
 export default function Product({title, imageSource, price}) {
   return (
-    <div className="produit">
+    <ProductStyled>
         <div className="image">
             <img src={imageSource} alt={title}/>
         </div>
     <div className="info-text">
-      <div className="description">
         <div className="title">{title}</div>
-        <div className="price">{price} €</div>
-        <button className="add-button">Ajouter</button>
+        <div className="description">
+            <div className="price">{price} €</div>
+            <button className="add-button">Ajouter</button>
       </div>
     </div>
-  </div>
+  </ProductStyled>
   )
 }
 
+const ProductStyled = styled.div`
+    background: red;
+    width: 200px;
+    height: 330px;
+    display: grid;
+    grid-template-rows: 65% 1fr;
+    padding-left: 20px;
+    padding-right: 20px;
+
+
+        
+        .image{
+            border: 1px solid yellow;
+            width: 100%;
+            height: auto;
+            margin-top: 50px;
+            
+            
+            img{
+                width: 100%;
+                height: 100%;
+                object-fit: contain;
+            }
+        }
+
+        .info-text{
+        border: 1px solid green;
+        
+        }
+`
