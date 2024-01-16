9a102781b18db13cca814c2340a9e91b93aaa761
loiclo <lecinloic07@gmail.com>
2024-01-16 14:29:32 +0100

    💄 F05: ORDER > MAIN > Product > add styling

--- src/components/pages/order/Main/Product.jsx
+++ src/components/pages/order/Main/Product.jsx
@@ -1,49 +1,96 @@
 import styled from "styled-components";
+import { theme } from '/src/theme/index.jsx';
+import PrimaryButton from "../../../reusable-ui/PrimaryButton";
 
 export default function Product({title, imageSource, price}) {
   return (
-    <ProductStyled>
+    <CardStyled>
         <div className="image">
             <img src={imageSource} alt={title}/>
         </div>
-    <div className="info-text">
+    <div className="text-info">
         <div className="title">{title}</div>
         <div className="description">
-            <div className="price">{price} €</div>
-            <button className="add-button">Ajouter</button>
+            <div className="left-description">{price} €</div>
+            <div className="right-desciption">
+              <PrimaryButton className="primary-button" label={"Ajouter"}/> 
+            </div>
       </div>
     </div>
-  </ProductStyled>
+  </CardStyled>
   )
 }
 
-const ProductStyled = styled.div`
-    background: red;
-    width: 200px;
-    height: 330px;
+const CardStyled = styled.div`
+  background: ${theme.colors.white};
+  width: 200px;
+  height: 300px;
+  display: grid;
+  grid-template-rows: 65% 1fr;
+  padding: 20px;
+  padding-bottom: 10px;
+  box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+  border-radius: ${theme.borderRadius.extraRound};
+
+  .image {
+    width: 100%;
+    height: auto;
+    margin-top: 30px;
+    margin-bottom: 20px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  }
+
+  .text-info{
     display: grid;
-    grid-template-rows: 65% 1fr;
-    padding-left: 20px;
-    padding-right: 20px;
+    grid-template-rows: 30% 70%;
+    padding: 5px;
 
+    .title {
+      margin: auto 0;
+      font-size: ${theme.fonts.P4};
+      position: relative;
+      bottom: 10px;
+      font-weight: ${theme.weights.bold};
+      color: ${theme.colors.dark};
+      text-align: left;
+      white-space: nowrap;
+      overflow: hidden;
+      width: 100%;
+      text-overflow: ellipsis;
+      font-family: "Amatic SC", cursive;
+    }
+    
+    .description{
+      display: grid;
+      grid-template-columns: 1fr 1fr;
 
-        
-        .image{
-            border: 1px solid yellow;
-            width: 100%;
-            height: auto;
-            margin-top: 50px;
-            
-            
-            img{
-                width: 100%;
-                height: 100%;
-                object-fit: contain;
-            }
+        .left-description{
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          font-weight: ${theme.weights.medium};
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          font-weight: ${theme.weights.medium};
+          color: ${theme.colors.primary};
         }
 
-        .info-text{
-        border: 1px solid green;
-        
+        .right-description {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        font-size: ${theme.fonts.P1};
+        .primary-button{
+          font-size: ${theme.fonts.XS};
+          cursor: pointer;
+          padding: 12px;
         }
-`
+      }
+    }
+  }`
